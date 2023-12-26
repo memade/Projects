feda910090e0f4885162f2cdad18dcb6be290299
memade <k34ubll5@outlook.com>
2023-12-26 17:13:22 +0800

    update on windows

--- tests/rust/tcp_server.rs
+++ tests/rust/tcp_server.rs
@@ -1,6 +1,18 @@
 use std::net::TcpListener;
 use std::io::prelude::*;
 
-fn main(){
-    
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+
+        let message = String::from_utf8_lossy(&buffer).to_string();
+        println!("Received message: {}", message);
+
+        stream.write(message.as_bytes()).unwrap();
+    }
 }
